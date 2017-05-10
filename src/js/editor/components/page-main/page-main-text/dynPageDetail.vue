57a1b6c298e00a01916e0c36732f39eddb994893
martintomas <martintomas@seznam.cz>
2017-05-10 10:56:47 +0200

    added scroller to page detail

--- src/js/editor/components/page-main/page-main-text/dynPageDetail.vue
+++ src/js/editor/components/page-main/page-main-text/dynPageDetail.vue
@@ -1,7 +1,7 @@
 <template>
 <div ref="dynPageDetail" class="modal">
     <div class='modal-middle-center' ref="dynPageDetailCenter">
-        <div class="modal-content">
+        <div class="modal-content modal-page-content">
             <template v-if='pageData != null'>
                 <div class="modal-header">
                     {{String.doTranslationEditor('page-zoom-modal-header',(pageData.data.pageNumber))}}&nbsp;
@@ -9,17 +9,25 @@
                     <span class="close float-right" @click='close'><i class="fa fa-close unactive-icon" aria-hidden="true" @click='close'></i></span>
                 </div>
                 <div class="modal-body">
-                    <div class='vertical-div float-left' v-if="closestSmallerPage != null" @click='previousPage'>
-                        <span class='modal-middle-center'>
-                            <i class="fa fa-chevron-left unactive-icon" aria-hidden="true"></i>
+                    <div class='vertical-div float-left'>
+                        <span class='active-vertical-div' v-if="closestSmallerPage != null" @click='previousPage'>
+                            <span class='modal-middle-center'>
+                                <i class="fa fa-chevron-left unactive-icon" aria-hidden="true"></i>
+                            </span>
                         </span>
                     </div>
-                    <div class='vertical-div float-right' v-if="closestBiggerPage != null" @click='nextPage'>
-                        <span class='modal-middle-center'>
-                            <i class="fa fa-chevron-right unactive-icon" aria-hidden="true"></i>
+                    <div class="page-detail-text float-left" ref="pageDetailWrapper">
+                        <div class="page-detail-scroller" ref="pageDetailScroller">
+                            <dyn-text-renderer :page-data='pageData' render-type='pageDetail'></dyn-text-renderer>
+                        </div>
+                    </div>
+                    <div class='vertical-div float-right'>
+                        <span class='active-vertical-div' v-if="closestBiggerPage != null" @click='nextPage'>
+                            <span class='modal-middle-center'>
+                                <i class="fa fa-chevron-right unactive-icon" aria-hidden="true"></i>
+                            </span>
                         </span>
                     </div>
-                    <dyn-text-renderer class='page-detail-text' :page-data='pageData' render-type='pageDetail'></dyn-text-renderer>
                 </div>
                 <div class="modal-footer">
                     {{String.doTranslationEditor('page-num',(pageData.data.pageNumber))}}
@@ -27,12 +35,13 @@
             </template>
         </div>
     </div>
-    
 </div>
 </template>
 
 <script>
+import IScroll from 'iscroll'
 import DynTextRenderer from 'editor/components/page-main/page-main-text/dynTextRenderer.js'
+import DynModal from 'editor/components/dyn-components/dynModal.vue'
 import {bus} from 'app.js'
 import {busEditor} from 'editor/services/defaults.js'
 
@@ -44,6 +53,9 @@ export default {
     },
     data() {
         return {
+            scrollWrapper: 'pageDetailWrapper',
+            scrollContainer: 'pageDetailScroller',
+            scroller: null,
             pageId:0,
         }
     },
@@ -67,7 +79,29 @@ export default {
             return null
         }
     },
-    mounted() {
+    watch: {
+        pageData(value) {
+            if(value != null && this.scroller === null) { //initialize scroller
+                this.$nextTick(() => { //have to wait till $refs is populated by new page data
+                    this.scroller = new IScroll(this.$refs[this.scrollWrapper], {
+                        mouseWheel: true,
+                        bounce: false,
+                        interactiveScrollbars: true,
+                        shrinkScrollbars: 'clip',
+                        scrollbars: 'custom',
+                    })
+                    setTimeout(() => {
+                        this.scroller.refresh() //actualize scroller based on new height
+                    }, 200)
+                })
+            } else {
+                setTimeout(() => {
+                    this.scroller.refresh() //actualize scroller based on new height
+                }, 200)
+            }
+        }
+    },
+    created() {
         busEditor.$on('show-page-detail',pageId => {
             this.pageId = pageId
             this.show()
@@ -78,6 +112,8 @@ export default {
                 this.close()
             }
         })
+    },
+    mounted() {
     },
     methods: {
         show() {
@@ -101,75 +137,54 @@ export default {
 </script>
 
 <style>
-.modal-content {
-    position: relative;
-    background-color: #fefefe;
-    margin:auto;
-    padding: 0;
-    border: 1px solid #888;
-    border-radius: 1.2rem;
-    width: 80%;
-    min-width:6rem;
-    height: 80vh;
-    min-height:5rem;
-    animation-name: animatetop;
-    animation-duration: 0.4s
-}
-
-@keyframes animatetop {
-    from {top: -300px; opacity: 0}
-    to {top: 0; opacity: 1}
-}
-
-.modal-header {
-    padding: 0.2rem 2%;
-    background-color: gray;
-    font-size: 130%;
-    font-weight: bold;
-    height: 1.5rem;
-    line-height: 1.5rem;
-    vertical-align: middle;
-    text-align:center;
-    border-top-left-radius: 1rem;
-    border-top-right-radius: 1rem;
-}
-.modal-body {
-    height: calc(100% - 3.9rem);
-    box-sizing: border-box;
-}
 .page-detail-text {
     margin:0 auto;
-    padding: 1rem 0;
-    width: 88%;
+    padding: 1rem 1%;
+    width: calc(98% - 4rem);
+    overflow: hidden;
+    box-sizing: border-box;
+    height:100%;
+        position: relative;
+}
+.page-detail-scroller {
+    /*z-index: 1;*/
+    width: calc(100% - 5px); /*left some place for scroller*/
+    transform: translateZ(0);
+    user-select: none;
+    text-size-adjust: none;
+    padding-bottom: 2rem;
 }
 .page-detail-text p{
     margin: 0;
 }
 .vertical-div {
-    width:5%;
-    font-size:200%;
+    width:2rem;
     height: 100%;
+}
+.active-vertical-div {
+    font-size:200%;
     display: table;
     text-align: center;
     cursor:pointer;
+    height:100%;
+    width:100%;
 }
-.vertical-div:hover {
+.active-vertical-div:hover {
     background-color: rgb(204, 204, 204);
 }
-.modal-footer {
-    position: absolute;
-    padding: 0.2rem 2%;
-    background-color: gray;
-    bottom: 0;
-    width: 100%;
+.modal-page-body {
+    height: calc(100% - 3.9rem);
     box-sizing: border-box;
-    text-align:center;
-    height: 2rem;
-    line-height: 2rem;
-    vertical-align: middle;
-    font-size: 140%;
-    font-weight: bold;
-    border-bottom-left-radius: 1rem;
-    border-bottom-right-radius: 1rem;
+}
+.modal-page-content {
+    width: 80%;
+    min-width:6rem;
+    height: 80vh;
+    min-height:5rem;
+}
+.close {
+    float: right;
+    font-size: 28px;
+    cursor:pointer;
 }
 </style>
